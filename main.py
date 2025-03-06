291866b10492c66372358c276a88f2c795d704ae
Kirill Matusevich <kirilenkich@gmail.com>
2025-03-07 00:49:38 +0300

    connection to db

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+from dotenv import load_dotenv
+import os
+import certifi
 from flask import Flask
+from flask import jsonify, request 
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+
+load_dotenv()
+
+MONGO_URI = os.getenv('MONGO_URI')
+# print(MONGO_URI)
 
 app = Flask(__name__)
+CORS(app)
+app.config["MONGO_URI"] = MONGO_URI
+mongo = PyMongo(app, tlsCAFile=certifi.where())
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods=['GET'])
+def find_user():
+    user = mongo.db.drivers.find_one({"name": "Dave"})
+    
+    return jsonify(user)  
