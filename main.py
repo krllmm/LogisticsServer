86716199477174cbed7ef3896ec0acc5bf641989
Kirill Matusevich <kirilenkich@gmail.com>
2025-05-16 19:24:28 +0300

    logists stuff

--- main.py
+++ main.py
@@ -82,8 +82,9 @@ def getDeliveries():
     if not login:
         return jsonify({"error": "Login is required"}), 400
 
-    user = mongo.db.drivers.find_one({"login": login}, {"_id": 0, "delivery": 1})
-    
+    user = mongo.db.drivers.find_one(
+        {"login": login}, {"_id": 0, "delivery": 1})
+
     def serialize_date(delivery):
         return {
             "from": delivery["from"],
@@ -92,7 +93,7 @@ def getDeliveries():
             "to_address": delivery["to_address"],
             "product_id": delivery["product_id"],
             "amount": delivery["amount"],
-            # "datetime": delivery["datetime"].isoformat()  
+            # "datetime": delivery["datetime"].isoformat()
         }
 
     deliveries = [serialize_date(e) for e in user.get("delivery", [])]
@@ -197,24 +198,24 @@ def seeddb_logists():
     try:
         mongo.db.logists.insert_one(
             {
-                "login": "Agent",
+                "first_name": "Игорь",
+                "second_name": "Алексеев",
+                "login": "",
                 "password": generate_password_hash("password"),
                 "super_rights": "true",
                 "storage_id": 1,
-                "drivers": [
-                    1, 2
-                ]
+                "drivers": []
             }
         )
         mongo.db.logists.insert_one(
             {
-                "login": "Agent_2",
-                "password": generate_password_hash("password_2"),
+                "first_name": "",
+                "second_name": "",
+                "login": "Agent 2",
+                "password": generate_password_hash("password 2"),
                 "super_rights": "false",
                 "storage_id": 1,
-                "drivers": [
-                    3, 4
-                ]
+                "drivers": []
             }
         )
         return jsonify({"message": "Logists created"}), 201
@@ -240,7 +241,7 @@ def loginLogist():
     if not check_password_hash(user["password"], password):
         return jsonify({"error": "Неверный пароль"}), 401
 
-    return jsonify({"message": "Вход выполнен успешно"}), 200
+    return jsonify({"message": "Вход выполнен успешно", "user": user}), 200
 
 
 @app.route('/getAllDrivers', methods=["GET"])
@@ -291,6 +292,7 @@ def addDriver():
     except Exception as e:
         return {"error": str(e)}, 500
 
+
 @app.route('/addDelivery', methods=["POST"])
 def addDelivery():
     try:
@@ -299,7 +301,7 @@ def addDelivery():
         fromAddress = data.get('from_address')
         toCity = data.get('to')
         toAddress = data.get('to_address')
-        
+
         product_id = data.get("product_id")
         amount = data.get("amount")
         iso_str = data.get("datetime")
@@ -314,9 +316,10 @@ def addDelivery():
         print(amount)
         print(date)
         print(id)
-        result = ''.join(random.choices(string.ascii_letters + string.digits, k=24))
+        result = ''.join(random.choices(
+            string.ascii_letters + string.digits, k=24))
         print(result)
-        
+
         if not id:
             return jsonify({"error": "id обязателен"}), 400
 
@@ -331,7 +334,7 @@ def addDelivery():
                 "product_id": product_id,
                 "amount": amount,
                 "date": date,
-            }}} 
+            }}}
         )
         if result.modified_count == 1:
             return jsonify({"message": "Перевозка добавлена"}), 200
@@ -340,24 +343,27 @@ def addDelivery():
     except Exception as e:
         return {"error": str(e)}, 500
 
+
 @app.route("/getAvailableDrivers", methods=["GET"])
 def getAvailableDrivers():
-    try:        
-        result = mongo.db.drivers.find({}, {"_id": 1, "first_name": 1, "second_name": 1})
+    try:
+        result = mongo.db.drivers.find(
+            {}, {"_id": 1, "first_name": 1, "second_name": 1})
 
-        items = [{"_id": str(doc["_id"]), "first_name": doc.get("first_name", ""), "second_name": doc.get("second_name", "")} for doc in result]
+        items = [{"_id": str(doc["_id"]), "first_name": doc.get(
+            "first_name", ""), "second_name": doc.get("second_name", "")} for doc in result]
 
         return jsonify(items)
     except Exception as e:
         return {"error": str(e)}, 500
-    
-    
+
+
 @app.route("/closeDelivery", methods=["POST"])
-def closeDelivery(): 
+def closeDelivery():
     data = request.get_json()
     deliveryId = data.get('deliveryId')
     driverLogin = data.get('driverLogin')
-    
+
     print(deliveryId, driverLogin)
     try:
         user = mongo.db.drivers.find_one({'login': driverLogin})
@@ -365,7 +371,8 @@ def closeDelivery():
             return 'User not found'
 
         # Найти нужный объект из массива
-        item_to_move = next((item for item in user.get('delivery', []) if str(item.get('id')) == deliveryId), None)
+        item_to_move = next((item for item in user.get(
+            'delivery', []) if str(item.get('id')) == deliveryId), None)
 
         if not item_to_move:
             return 'Item not found in user'
@@ -382,7 +389,8 @@ def closeDelivery():
         return 'Item moved successfully'
     except Exception as e:
         return {"error": str(e)}, 500
-    
+
+
 @app.route("/getDeliveryHistory", methods=["GET"])
 def getDeliveryHistory():
     history = mongo.db.delivery_history.find()
@@ -391,3 +399,99 @@ def getDeliveryHistory():
         return jsonify(history)
     else:
         return jsonify({"error": "Error happend on server"}), 404
+
+
+@app.route("/getProducts", methods=["GET"])
+def getProducts():
+    products = mongo.db.products.find()
+
+    if products:
+        return jsonify(products)
+    else:
+        return jsonify({"error": "Error happend on server"}), 404
+
+
+@app.route("/getLogists", methods=["GET"])
+def getLogists():
+    logists = mongo.db.logists.find()
+
+    result = [serialize_doc(item) for item in logists]
+
+    if result:
+        return jsonify(result)
+    else:
+        return jsonify({"error": "Error happend on server"}), 404
+
+
+@app.route('/addLogist', methods=["POST"])
+def addLogist():
+    try:
+        data = request.get_json()
+        name = data.get('first_name')
+        second_name = data.get('second_name')
+        login = data.get('login')
+        password = data.get('password')
+        super = data.get("super_rights")
+
+        print(name)
+        print(second_name)
+        print(login)
+        print(password)
+        print(super)
+
+        mongo.db.logists.insert_one(
+            {
+                "login": login,
+                "password": generate_password_hash(password),
+                "first_name": name,
+                "second_name": second_name,
+                "super_rights": super,
+                # "storage_id": 1,
+                "drivers": []
+            })
+        return jsonify({"message": "Driver is created"}), 201
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+
+def serialize_doc(doc):
+    doc['_id'] = str(doc['_id'])
+    return doc
+
+
+@app.route("/getFreeDrivers", methods=["GET"])
+def getFreeDrivers():
+    try:
+
+        drivers = mongo.db.drivers.find({'$or': [
+            {'logist_id': {'$exists': False}},
+            {'logist_id': ''},
+        ]}, {"_id": 1, "first_name": 1, "second_name": 1})
+        
+        result = [serialize_doc(item) for item in drivers]
+
+        return jsonify(result)
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+@app.route("/assignDriverToLogist", methods=["POST"])
+def assignDriverToLogist():
+    try: 
+        data = request.get_json()
+        
+        driverId = ObjectId(data.get("driverId"))
+        logistId = ObjectId(data.get("logistId"))
+        
+        mongo.db.drivers.update_one(
+            {'_id': driverId, 'logist_id': {'$exists': False}},  
+            {'$set': {'logist_id': data.get("logistId")}}
+        )
+        
+        mongo.db.logists.update_one(
+            {'_id': logistId},
+            {'$addToSet': {'drivers': data.get("driverId")}}  
+        )
+            
+        return jsonify({'status': 'updated'}), 200
+    except Exception as e:
+        return {"error": str(e)}, 500
