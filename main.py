68e474180cc3c9253a0f6f046e4e83ca06f2bb4e
Kirill Matusevich <kirilenkich@gmail.com>
2025-06-05 14:37:08 +0300

    added port

--- main.py
+++ main.py
@@ -533,3 +533,7 @@ def assignDriverToLogist():
         return jsonify({'status': 'updated'}), 200
     except Exception as e:
         return {"error": str(e)}, 500
+    
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
